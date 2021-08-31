20570340daced53759fd1e5a47c5fb3fc44480da
darodriguezz <darodriguezz@outlook.com>
2021-08-30 23:01:21 -0500

    Commit-ejercicios

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,3 @@
     ===== Código de TypeScript =====
 */
 
-class Heroe{
-
-    /* alterEgo: string;
-    edad: number;
-    nombreReal: string; */
-
-    constructor( 
-        public alterEgo: string,
-        public edad: number,
-        public nombreReal: string){}
-
-}
-
-const iroman = new Heroe('Ironman' , 45 ,'Tony');
-
-console.log(iroman);
